db96a46d0ca82517f2e7ebb35e924b9583245a2e
Franck Pachot <mail@pachot.net>
2025-02-26 06:53:51 +0000

    not clustered index

--- bench01/init.js
+++ bench01/init.js
@@ -1,7 +1,6 @@
 db.jsontable.drop();
 db.runCommand( {
-   create: "jsonbench",
-   clusteredIndex: { "key": { "_id": 1 }, "unique": true, "name": "jsonbench clustered key" }
+   create: "jsonbench"
 } )
 db.jsonbench.createIndex({ attr1:1 });
 db.jsonbench.find({attr1:{$gt: "M" }}).sort({attr1:1}).limit(5).explain().queryPlanner.winningPlan;
